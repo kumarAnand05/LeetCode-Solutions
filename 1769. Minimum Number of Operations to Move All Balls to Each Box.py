de0d53a34e4000a7bd0af541fe17f90bc6cca76d
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2022-11-07 20:18:29 +0530

    Update 1769. Minimum Number of Operations to Move All Balls to Each Box.py

--- 1769. Minimum Number of Operations to Move All Balls to Each Box.py
+++ 1769. Minimum Number of Operations to Move All Balls to Each Box.py
@@ -1,6 +1,3 @@
 class Solution:
     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        x=[t[0] for t in points]
-        x.sort()
-        diff=[x[n+1]-x[n] for n in range(len(x)-1)]
-        return max(diff)
+        return max([t - s for s, t in zip(sorted([r[0] for r in points]), sorted([r[0] for r in points])[1:])])
