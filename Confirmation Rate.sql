8a393912a8fb9f4ef440b3e0e01010ad0f987a1d
Anand Kumar <kumar.anand05@yahoo.com>
2025-08-17 18:19:06 +0530

    Create Confirmation Rate.sql

---
+++ Confirmation Rate.sql
@@ -0,0 +1,9 @@
+select s.user_id, 
+round(sum(case 
+    when c.action = 'confirmed' then 1
+    else 0
+    end)/count(*),2) as confirmation_rate
+ from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
